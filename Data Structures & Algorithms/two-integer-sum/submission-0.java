3910653d43b2165127860a26d06920af24e0eca9
Anna Margery <131193379+AnnaMargery@users.noreply.github.com>
2026-07-02 22:35:14 +0200

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> valueToIndex = new HashMap<>();
+        int[] indexes = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int compliment = target - nums[i];
+            if (valueToIndex.containsKey(compliment)) {
+                indexes[0] = valueToIndex.get(compliment);
+                indexes[1] = i;
+            }
+            valueToIndex.put(nums[i], i);
+        }
+        return indexes;
+    }
+}
