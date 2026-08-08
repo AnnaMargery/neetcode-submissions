36d7d4e9901fb9659984578f789b659f863f49ba
Anna Margery <131193379+AnnaMargery@users.noreply.github.com>
2026-08-08 18:03:12 +0200

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        int res = right;
+
+        while (left <= right) {
+            int k = left + (right - left) / 2;
+            int time = 0;
+
+            for (int pile : piles) {
+                time += (pile + k - 1) / k;
+            }
+            if (time <= h) {
+                res = k;
+                right = k - 1;
+            } else {
+                left = k + 1;
+            }
+        }
+        return res;
+    }
+}
